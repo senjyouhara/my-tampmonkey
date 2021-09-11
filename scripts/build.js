d623e0e00468ca8de390a1e48d2bee187ef2877a
hanekawa <ayano@loli.com.co>
2021-09-11 17:14:12 +0800

    打包结构修改

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,34 @@
-const build = require("./utils/build-one");
 const terminal = require("./utils/terminal");
+const { webpackConfigHandler } = require('../config/config')
+const webpack = require('webpack');
 
-const isDev = process.argv.includes("-D");
 const name = process.argv[process.argv.length - 1];
 
-build(name, false)
+
+function build(name) {
+    return new Promise(async (resolve, reject) => {
+
+        const webpackConfig = webpackConfigHandler()
+        const complier = webpack(webpackConfig);
+
+        complier.run((err, stats) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            // 遍历stats，检查有没有错误
+            for (const module of stats.compilation.modules) {
+                if (module.errors && module.errors.length > 0) {
+                    reject(module.errors);
+                    return;
+                }
+            }
+            resolve({name, stats});
+        });
+    });
+}
+
+build(name)
   .then((res) => {
     terminal.assets(res.stats.compilation.assets);
   })
