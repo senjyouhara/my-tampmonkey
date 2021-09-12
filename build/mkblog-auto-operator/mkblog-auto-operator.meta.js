90833e8171da8e7240fde10e595ccab3e4c6e629
hanekawa <ayano@loli.com.co>
2021-09-12 17:18:09 +0800

    更新配置

---
+++ build/mkblog-auto-operator/mkblog-auto-operator.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name mkblog_auto
+// @namespace lab.mkblog.cn
+// @description mkblog小游戏在线自动操作面板
+// @author ayano
+// @include http://lab.mkblog.cn/adarkroom/
+// @grant GM_addStyle
+// @version 0.0.1
+// @updateURL https://userscript.firefoxcn.net/js/mkblog-auto-operator.meta.js
+// @downloadURL https://userscript.firefoxcn.net/js/mkblog-auto-operator.user.js
+// ==/UserScript==
